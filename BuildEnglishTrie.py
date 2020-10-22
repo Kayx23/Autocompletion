3196ebcaaef68fed527b13716380d3dcf38391f7
Traky <dengk7@mcmaster.ca>
2020-10-22 03:41:12 -0400

    wrapped in function

--- BuildEnglishTrie.py
+++ BuildEnglishTrie.py
@@ -1,9 +1,13 @@
 from trie import Trie
 
-trie = Trie()
 
-# https://github.com/dwyl/english-words/blob/master/words_alpha.txt
-Words = open('EnglishWords/words_alpha.txt', 'r')
+def loadTrie() -> Trie:
+    trie = Trie()
 
-for word in Words:
-    trie.addWord(word.rstrip('\n'))  # had to strip \n
+    # taken from https://github.com/dwyl/english-words/blob/master/words_alpha.txt
+    Words = open('EnglishWords/words_alpha.txt', 'r')
+
+    for word in Words:
+        trie.addWord(word.rstrip('\n'))  # had to strip \n
+
+    return trie
