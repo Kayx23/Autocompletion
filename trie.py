3b11e74d7dd2f85a2fce76cd7cb92d204156a282
Traky <dengk7@mcmaster.ca>
2020-10-22 22:08:59 -0400

    renamed var

--- trie.py
+++ trie.py
@@ -13,24 +13,24 @@ class Trie:
 
     def addWord(self, word: str) -> None:
         """Adds a word."""
-        curr_trie = self.root
+        curr_node = self.root
         for letter in word:
-            if letter not in curr_trie:
+            if letter not in curr_node:
                 # create a dictionary under that letter
-                curr_trie[letter] = {}
+                curr_node[letter] = {}
             # move down one level
-            curr_trie = curr_trie[letter]
+            curr_node = curr_node[letter]
         # now we're at the end of the word
-        curr_trie["*"] = "*"
+        curr_node["*"] = "*"
 
     def existWord(self, word: str) -> None:
         """Checks if a word exist in the trie; returns a Boolean value."""
-        curr_trie = self.root
+        curr_node = self.root
         for letter in word:
-            if letter not in curr_trie:
+            if letter not in curr_node:
                 return False
             else:
                 # move down one level
-                curr_trie = curr_trie[letter]
+                curr_node = curr_node[letter]
         # finally make sure the end of word mark is there
-        return "*" in curr_trie
+        return "*" in curr_node
